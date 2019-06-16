006a94a89b58f42677f4bda56d30f4347270ae7f
hongkongers69 <51717229+hongkongers69@users.noreply.github.com>
2019-06-16 23:53:38 +0800

    Update liveData.js

--- liveData.js
+++ liveData.js
@@ -1,24 +1,24 @@
 var liveData = [
     {
-        "url": "https://www.facebook.com/now.comNews/videos/2135179276550827/",
-        "startTime": "",
-        "endTime": "",
-        "tv": "now"
-    },
-    {
-        "url": "https://www.facebook.com/standnewshk/videos/461997384533790/",
+        "url": "https://www.facebook.com/standnewshk/videos/657330324737543/",
         "startTime": "",
         "endTime": "",
         "tv": "stand"
     },
     {
-        "url": "https://www.facebook.com/icablenews/videos/942098666131952/",
+        "url": "https://www.facebook.com/standnewshk/videos/358023401783002/",
+        "startTime": "",
+        "endTime": "",
+        "tv": "stand"
+    },
+    {
+        "url": "https://www.facebook.com/icablenews/videos/638054990027107/",
         "startTime": "",
         "endTime": "",
         "tv": "icable"
     },
     {
-        "url": "https://www.facebook.com/hk.nextmedia/videos/2385543361676096/",
+        "url": "https://www.facebook.com/hk.nextmedia/videos/310378423179686/",
         "startTime": "",
         "endTime": "",
         "tv": "apple"
