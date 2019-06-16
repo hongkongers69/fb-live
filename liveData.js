0ac8f5ea793a77e48ba8d90a2b72b90d00e3dbcb
hongkongers69 <51717229+hongkongers69@users.noreply.github.com>
2019-06-16 15:06:54 +0800

    Update liveData.js

--- liveData.js
+++ liveData.js
@@ -1,10 +1,10 @@
 var liveData = [
-    <—{
-        "url": "https://www.facebook.com/standnewshk/videos/2257456607641124/",
-        "startTime": "",
-        "endTime": "",
-        "tv": "stand"
-    },—>
+    //{
+    //    "url": "https://www.facebook.com/standnewshk/videos/2257456607641124/",
+     //   "startTime": "",
+     //   "endTime": "",
+     //   "tv": "stand"
+    //},
     {
         "url": "https://www.facebook.com/now.comNews/videos/2135179276550827/",
         "startTime": "",
