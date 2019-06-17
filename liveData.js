0a075b66e1e7c52b3b41809d6070a1314290518e
hongkongers69 <51717229+hongkongers69@users.noreply.github.com>
2019-06-17 21:22:33 +0800

    Add files via upload

--- liveData.js
+++ liveData.js
@@ -1,8 +1,8 @@
 var liveData = [
     {
-        "url": "https://www.facebook.com/RTHKVNEWS/videos/687719445000854/",
+        "url": "https://www.facebook.com/hk.nextmedia/videos/322307578703225/",
         "startTime": "",
         "endTime": "",
-        "tv": "rthk"
+        "tv": "apple"
     }
 ]
