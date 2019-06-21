069cd324436b56a3cf099d0a34bbd5b030b43819
hongkongers69 <51717229+hongkongers69@users.noreply.github.com>
2019-06-21 09:03:50 +0800

    Update liveData.js

--- liveData.js
+++ liveData.js
@@ -5,18 +5,6 @@ var liveData = [
     //     "endTime": "",
     //     "tv": "apple"
     // }
-    {
-        "url": "https://www.facebook.com/standnewshk/videos/838231946576101/",
-        "startTime": "",
-        "endTime": "",
-        "tv": "stand"
-    },
-    {
-        "url": "https://www.facebook.com/icablenews/videos/1389013357906269/",
-        "startTime": "",
-        "endTime": "",
-        "tv": "icable"
-    }
     // {
     //     "url": "https://www.facebook.com/standnewshk/videos/838231946576101/",
     //     "startTime": "",
@@ -29,4 +17,47 @@ var liveData = [
     //     "endTime": "",
     //     "tv": "now"
     // }
+    
+    {
+        "url": "https://www.facebook.com/passiontimes/videos/2346266568994938/",
+        "startTime": "",
+        "endTime": "",
+        "tv": "passionTimes"
+    }
+    
+    
+    {
+        "url": "https://www.facebook.com/RTHKVNEWS/videos/444557129455961/",
+        "startTime": "",
+        "endTime": "",
+        "tv": "RTHK"
+    }
+
+    {
+        "url": "https://www.facebook.com/epochtimes.taiwan/videos/309874573234900/",
+        "startTime": "",
+        "endTime": "",
+        "tv": "thEpochTimes"
+    }
+
+    {
+        "url": "https://www.facebook.com/hk.nextmedia/videos/434764160638354/",
+        "startTime": "",
+        "endTime": "",
+        "tv": "antiDogAnimation"
+    }
+
+    {
+        "url": "https://www.facebook.com/standnewshk/videos/838231946576101/",
+        "startTime": "",
+        "endTime": "",
+        "tv": "stand"
+    },
+    {
+        "url": "https://www.facebook.com/icablenews/videos/1389013357906269/",
+        "startTime": "",
+        "endTime": "",
+        "tv": "icable"
+    }
+    
 ]
