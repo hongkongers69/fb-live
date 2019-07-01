ae8628cebcc102207d5ea01622feb3bd5c65c7ed
hongkongers69 <51717229+hongkongers69@users.noreply.github.com>
2019-07-01 14:14:22 +0800

    Add files via upload

--- liveData.js
+++ liveData.js
@@ -1,38 +1,39 @@
 var liveData = [
     {
-        "url": "https://www.facebook.com/standnewshk/videos/3091856017492974/",
+        "url": "https://www.facebook.com/standnewshk/videos/3091856017492974",
         "startTime": "",
         "endTime": "",
         "tv": "stand"
     },
     {
-        "url": "https://www.facebook.com/standnewshk/videos/427818741400295/",
+        "url": "https://www.facebook.com/standnewshk/videos/2485806484981567",
         "startTime": "",
         "endTime": "",
         "tv": "stand"
     },
     {
-        "url": "https://www.facebook.com/now.comNews/videos/331012481153584/",
+        "url": "https://www.facebook.com/standnewshk/videos/352330425442893",
+        "startTime": "",
+        "endTime": "",
+        "tv": "stand"
+    },
+    
+    {
+        "url": "https://www.facebook.com/now.comNews/videos/331012481153584",
         "startTime": "",
         "endTime": "",
         "tv": "now"
     },
     {
-        "url": "https://www.facebook.com/hk01.news/videos/490040748230352/",
+        "url": "https://www.facebook.com/hk01.news/videos/490040748230352&autoplay=false",
         "startTime": "",
         "endTime": "",
         "tv": "hk01"
     },
     {
-        "url": "https://www.facebook.com/inmediahk/videos/430900794173040/",
+        "url": "https://www.facebook.com/inmediahk/videos/430900794173040&autoplay=false",
         "startTime": "",
         "endTime": "",
         "tv": "inmedia"
-    },
-    {
-        "url": "https://www.facebook.com/socrec/videos/469996113572033/",
-        "startTime": "",
-        "endTime": "",
-        "tv": "socrec"
     }
 ]
