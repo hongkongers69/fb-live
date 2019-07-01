da88726ff63bd4d442cfbba9b70decbe4368aba9
hongkongers69 <51717229+hongkongers69@users.noreply.github.com>
2019-07-01 15:49:47 +0800

    Add files via upload

--- liveData.js
+++ liveData.js
@@ -11,18 +11,24 @@ var liveData = [
         "endTime": "",
         "tv": "stand"
     },
+    
     {
-        "url": "https://www.facebook.com/standnewshk/videos/2138266369803771/",
+        "url": "https://www.facebook.com/hk.nextmedia/videos/2296338913951065/",
         "startTime": "",
         "endTime": "",
-        "tv": "stand"
+        "tv": "apple"
     },
-    
     {
         "url": "https://www.facebook.com/now.comNews/videos/331012481153584",
         "startTime": "",
         "endTime": "",
         "tv": "now"
+    },
+    {
+        "url": "https://www.facebook.com/standnewshk/videos/2138266369803771&autoplay=false",
+        "startTime": "",
+        "endTime": "",
+        "tv": "stand"
     },
     {
         "url": "https://www.facebook.com/hk01.news/videos/490040748230352&autoplay=false",
