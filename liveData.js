b910f4f79ff01d3d179ddaae8277233792290470
hongkongers69 <51717229+hongkongers69@users.noreply.github.com>
2019-07-01 13:08:36 +0800

    Update liveData.js

--- liveData.js
+++ liveData.js
@@ -1,32 +1,8 @@
 var liveData = [
     {
-        "url": "https://www.facebook.com/hk01.news/videos/468930630562549/",
+        "url": "https://www.facebook.com/standnewshk/videos/1258868860953051/",
         "startTime": "",
         "endTime": "",
-        "tv": "hk01"
-    },
-    {
-        "url": "https://www.facebook.com/hk.nextmedia/videos/691117718027078/",
-        "startTime": "",
-        "endTime": "",
-        "tv": "apple"
-    },
-    {
-        "url": "https://www.facebook.com/RTHKVNEWS/videos/1813326712146700/",
-        "startTime": "",
-        "endTime": "",
-        "tv": "rthk"
+        "tv": "stand"
     }
-    // {
-    //     "url": "https://www.facebook.com/standnewshk/videos/2280994748817432/",
-    //     "startTime": "",
-    //     "endTime": "",
-    //     "tv": "stand"
-    // },
-    // {
-    //     "url": "https://www.facebook.com/standnewshk/videos/424147304847688/",
-    //     "startTime": "",
-    //     "endTime": "",
-    //     "tv": "stand"
-    // }
 ]
