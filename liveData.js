7c7ac32da4566d8e76648a11e41c356b2d865d31
hongkongers69 <51717229+hongkongers69@users.noreply.github.com>
2019-07-06 15:18:18 +0800

    Add files via upload

--- liveData.js
+++ liveData.js
@@ -1,38 +1,14 @@
 var liveData = [
     {
-        "url": "https://www.facebook.com/RTHKVNEWS/videos/2293152654235902/",
-        "startTime": "",
-        "endTime": "",
-        "tv": "rthk"
-    },
-    {
-        "url": "https://www.facebook.com/standnewshk/videos/460293998090540/",
-        "startTime": "",
-        "endTime": "",
-        "tv": "stand"
-    },
-    {
-        "url": "https://www.facebook.com/now.comNews/videos/2087688258002843/",
-        "startTime": "",
-        "endTime": "",
-        "tv": "now"
-    },
-    {
-        "url": "https://www.facebook.com/hk.nextmedia/videos/402262060379216/",
-        "startTime": "",
-        "endTime": "",
-        "tv": "apple"
-    },
-    {
-        "url": "https://www.facebook.com/hk01.news/videos/479481385959546&autoplay=false",
+        "url": "https://www.facebook.com/hk01.news/videos/719609658489722/",
         "startTime": "",
         "endTime": "",
         "tv": "hk01"
     },
     {
-        "url": "https://www.facebook.com/standnewshk/videos/428774301074423&autoplay=false",
+        "url": "https://www.facebook.com/hk.nextmedia/videos/880196155700546/",
         "startTime": "",
         "endTime": "",
-        "tv": "stand"
+        "tv": "apple"
     }
 ]
